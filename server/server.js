9bff74e835e87bf83f0300a1582299256789f974
yuetang1 <yue.tang.2@stonybrook.edu>
2026-03-04 17:14:09 -0500

    setup express server and connection to local db

--- server/server.js
+++ server/server.js
@@ -2,27 +2,27 @@
 // pw: W4vjg2MjSnHWEdjK
 // run these commands to see if it connects: npm install mongodb -> node server.js
 // success => "Pinged your deployment. You successfully connected to MongoDB!" in the terminal
+//const uri = "mongodb+srv://yufanghsu_db_user:W4vjg2MjSnHWEdjK@cluster0.72vft27.mongodb.net/?appName=Cluster0";
 
-import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://yufanghsu_db_user:W4vjg2MjSnHWEdjK@cluster0.72vft27.mongodb.net/?appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+let mongoose = require("mongoose");
+
+const mongoDB = "mongodb://127.0.0.1/cse416-FantasyDraftingTool";
+
+mongoose.connect(mongoDB)
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("connected", function () {
+  console.log("Connected to database");
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', async (req, res) => {
+  res.send("Hello World!");
+})
+
+app.listen(port, () => {
+  console.log(`fantasyDraftingTool server listening on port ${port}`)
+})
