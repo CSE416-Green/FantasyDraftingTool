427b4bf06d2ddfc25e80b0e6fb93680632227630
yuetang1 <yue.tang.2@stonybrook.edu>
2026-03-05 22:35:24 -0500

    open notification feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './css/App.css'
 import MainPage from './MainPage'
-import TeamRoster from './TeamRoster'
-import PlayerPool from './PlayerPool'
-import Note from './Note'
-import LeagueConfiguration from './LeagueConfig'
-import UpdatePlayerEligibility from './UpdatePlayerEligibility'
-import AddPlayerToPool from './AddPlayerToPool'
-import PlayerNews from './PlayerNews'
 import {
   QueryClient,
   QueryClientProvider,
@@ -18,33 +9,9 @@ import {
 const queryClient = new QueryClient()
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [team, setTeam] = useState("Team 1")
-  const [view, setView] = useState("roster") // roster or farm
-
-  function handleTeamChange(newTeam) {
-    setTeam(newTeam);
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <div>Welcome, Green.</div> */}
       <MainPage />
-      <TeamRoster />
-      <PlayerPool
-        team={team}
-        view={view}
-        onTeamChange={setTeam}
-        onRosterPlayers={() => setView("roster")} 
-        onFarmPlayers={() => setView("farm")}
-      />
-      <Note />
-
-      <div>Domonstration Purpose, not actually on this page...</div>
-      <LeagueConfiguration />
-      <UpdatePlayerEligibility />
-      <AddPlayerToPool />
-      <PlayerNews />
     </QueryClientProvider>
   )
 }
