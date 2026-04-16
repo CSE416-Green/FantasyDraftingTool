b08c814415285bb5887e50faf178445e06d0d301
Nikita Vorotnikov <nikita.vorotnikov36@gmail.com>
2026-04-16 15:52:11 -0400

    Fixed entry point and CSS import paths.

--- client/src/features/player-news/PlayerNews.jsx
+++ client/src/features/player-news/PlayerNews.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import "./css/playerNews.css";
+import "../../css/playerNews.css";
 import Drawer from './Drawer';
 
 export default function PlayerNews() {
