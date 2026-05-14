d9c2d644d5e2d4d7338c1b54c3bd40835feb0dab
yuetang1 <yue.tang.2@stonybrook.edu>
2026-05-14 16:01:50 -0400

    connect to ws through nginx

--- server/routes/receiveNotif.js
+++ server/routes/receiveNotif.js
@@ -30,14 +30,17 @@ export function setupWebSocket(server) {
 
 export function connectToReceiveNotifications() {
     console.log("Attempting to connect to WebSocket server for notifications...");
-    const client = io("wss://fantasybaseballplayerstatsapi.onrender.com", 
+    const client = io("wss://fantasybaseballgateway-nginx.onrender.com", 
     {
-        path: "/getPlayerNews",
+        path: "/ws/getPlayerNews",
         reconnection: true,              // enable retry
         reconnectionAttempts: Infinity,  // retry forever
         reconnectionDelay: 1000,         // initial delay
         reconnectionDelayMax: 5000,      // max delay cap
-        timeout: 20000                   // connection timeout
+        timeout: 20000,                 // connection timeout
+        extraHeaders: {
+          "Authorization": `apikey ${process.env.API_KEY}`,
+        },
     });
 
     client.on("connect", () => {
